4da47874c05def92d5f2c5793ffddb7e49702281
macbook <macbook2015@Macbooks-MacBook-Pro.local>
2019-12-24 11:48:19 +0700

    init project counting

---
+++ counting/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  counting
+//
+//  Created by macbook2015 on 24/12/19.
+//  Copyright © 2019 YMT. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
